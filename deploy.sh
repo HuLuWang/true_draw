d85837b72dc246ce81d3b98dee1538be79fbfd2d
whj <whj13004@gmail.com>
2019-09-03 17:04:45 +0800

    fix deploy.sh

---
+++ deploy.sh
@@ -0,0 +1,22 @@
+#! /bin/bash
+app=$1
+cd /root/workspace/
+if [ ! -d "${app}" ];then
+    echo "克隆项目${app}"
+    git clone -b $app git@github.com:HuLuWang/wang_projiect.git $app
+    cd ${app}
+else
+    cd ${app}
+    echo "拉取项目${app}"
+    git pull origin $app
+fi
+branch='master'
+echo ${branch}
+web=$(docker-compose ps | grep $branch | awk '{print $1}')
+if [ -z $web ];then
+    echo "构建容器: ${web}"
+    docker-compose up -d
+else
+    echo "重启容器: ${web}"
+    docker-compose restart
+fi
