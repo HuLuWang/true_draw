96f6ac7cb415c68354e6e7afdc7e81fb4efe5cbb
汪先森 <huijian.wang@hpb.io>
2020-09-15 17:47:36 +0800

    奖池设计

--- app/models/lottery_user.rb
+++ app/models/lottery_user.rb
@@ -4,9 +4,10 @@ class LotteryUser < ApplicationRecord
   belongs_to :user
 
   def self.generate option
-    LotteryUser.create!(
+    lottery = LotteryUser.create!(
                    user_id: option[:user_id],
                    lottery_id: option[:lottery_id]
     )
+    $redis.rpush("USER_POOL:#{lottery.id}", option[:user_id])
   end
 end
--- app/service/lottery_service.rb
+++ app/service/lottery_service.rb
@@ -12,7 +12,7 @@ class LotteryService
 
   # 获取奖池
   def user_pool
-    @user_pool = $redis.get("USER_POOL:#{@lottery.id}")
+    @user_pool =  $redis.lrange("USER_POOL:#{lottery.id}", 1, -1)
   end
 
   #开奖
